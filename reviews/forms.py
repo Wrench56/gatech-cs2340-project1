6eb5e5b2c45af4f5f08eb426fd200febf6593ea8
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-26 14:33:42 -0500

    feat(reviews): hide fields `mid` and `rid` from editor form

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,4 +5,6 @@ class ReviewForm(forms.Form):
     title = forms.CharField(label="Title", max_length=63)
     comment = forms.CharField(label="Comment", max_length=65535)
     mid = forms.CharField(required=False)
+    mid.widget = mid.hidden_widget()
     rid = forms.CharField(required=False)
+    rid.widget = rid.hidden_widget()
