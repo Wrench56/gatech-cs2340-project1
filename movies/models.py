4205946de78ece0f590c416327ac8125d8d5ed4c
Mark Devenyi <markdevenyidev@gmail.com>
2026-02-01 19:33:06 -0500

    feat(movies): add default movie and person images

--- movies/models.py
+++ movies/models.py
@@ -1,13 +1,15 @@
 from django.db import models
- 
+
 import uuid
+
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='people')
+    image = models.ImageField(upload_to='people', blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.name}'
+
 
 class Movie(models.Model):
     MOVIE_GENRE = (
@@ -15,6 +17,12 @@ class Movie(models.Model):
         ('DRAMA', 'Drama'),
         ('COMEDY', 'Comedy'),
         ('HORROR', 'Horror'),
+        ('SCI-FI', 'Sci-Fi'),
+        ('ROMANCE', 'Romance'),
+        ('THRILLER', 'Thriller'),
+        ('FANTASY', 'Fantasy'),
+        ('DOCUMENTARY', 'Documentary'),
+        ('ANIMATION', 'Animation'),
     )
 
     title = models.CharField(max_length=255)
@@ -23,7 +31,7 @@ class Movie(models.Model):
     genre = models.CharField(choices=MOVIE_GENRE)
     director = models.ForeignKey(Person, models.CASCADE, related_name='director')
     summary = models.CharField(max_length=8195)
-    image = models.ImageField(upload_to='movies')
+    image = models.ImageField(upload_to='movies', blank=True, null=True)
     actors = models.ManyToManyField(Person, related_name='actor')
     price = models.FloatField()
     mid = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
